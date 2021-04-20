afd8129c7c952ab1195812a83a650cd071f741e0
mhemt <74146735+mhemt@users.noreply.github.com>
2021-04-20 11:11:07 +0300

    Add API RPC for generating URL for downloading from storage

--- src/api/storage.sql
+++ src/api/storage.sql
@@ -19,3 +19,27 @@ BEGIN
     RETURN result;
 END;
 $$ LANGUAGE PLPGSQL VOLATILE;
+
+DROP FUNCTION IF EXISTS api.storage_generate_presigned_get(jsonb);
+CREATE OR REPLACE FUNCTION api.storage_generate_presigned_get(data jsonb)
+RETURNS jsonb AS $$
+DECLARE
+    credentials  jsonb;
+    user_info    jsonb;
+    result       jsonb;
+BEGIN
+    SELECT reclada_user.auth_by_token(data->>'access_token') INTO user_info;
+    data := data - 'access_token';
+
+    IF(NOT(reclada_user.is_allowed(user_info, 'generate presigned post', '{}'))) THEN
+        RAISE EXCEPTION 'Insufficient permissions: user is not allowed to %', 'generate presigned post';
+    END IF;
+
+    -- TODO: check user's permissions for reclada object access
+
+    SELECT reclada_object.list('{"class": "S3Config", "attrs": {}}')::jsonb -> 0 INTO credentials;
+
+    SELECT reclada_storage.s3_generate_presigned_get(data, credentials) INTO result;
+    RETURN result;
+END;
+$$ LANGUAGE PLPGSQL VOLATILE;
--- src/recladastorage_functions.sql
+++ src/recladastorage_functions.sql
@@ -30,3 +30,32 @@ RETURNS JSONB AS $$
 
     return json.dumps(response)
 $$ LANGUAGE 'plpython3u';
+
+CREATE OR REPLACE FUNCTION reclada_storage.s3_generate_presigned_get(data JSONB, credentials JSONB)
+RETURNS JSONB AS $$
+    import json
+
+    import boto3
+
+    json_data = json.loads(data)
+    json_credentials = json.loads(credentials)["attrs"]
+
+    s3_client = boto3.client(
+        service_name="s3",
+        endpoint_url=json_credentials.get("endpointURL"),
+        region_name=json_credentials.get("regionName"),
+        aws_access_key_id=json_credentials["accessKeyId"],
+        aws_secret_access_key=json_credentials["secretAccessKey"],
+    )
+
+    url = s3_client.generate_presigned_url(
+        ClientMethod="get_object",
+        Params={
+            "Bucket": json_credentials["bucketName"],
+            "Key": json_data["object_name"],
+        },
+        ExpiresIn=3600,
+    )
+
+    return json.dumps({"url": url})
+$$ LANGUAGE 'plpython3u';
