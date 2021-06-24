cea3d3baec00e17e5f20fa621b8b51db0ca97fda
Nikolay Soms <nsoms@users.noreply.github.com>
2021-06-24 18:45:10 +0300

    Update recladaobject_trigger_functions.sql
    updated runner id

--- src/recladaobject_trigger_functions.sql
+++ src/recladaobject_trigger_functions.sql
@@ -34,7 +34,7 @@ BEGIN
                 "class": "Job",
                 "attrs": {
                     "task": "c94bff30-15fa-427f-9954-d5c3c151e652",
-                    "runner": "d0320887-5526-4371-bae4-61bcd5e325e3",
+                    "runner": "2bc7d3bb-a23a-416d-a8ec-c8df9968a900",
                     "status": "pending",
                     "type": "shell",
                     "command": "./run_pipeline.sh",
