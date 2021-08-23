bd8d927f653373c09f90686f607879e253df3cca
AlexandraVin <81476464+AlexandraVin@users.noreply.github.com>
2021-08-23 17:41:50 +0300

    * improve api.storage_generate_presigned_get with Lambda function (#53)

--- update/up_script.sql
+++ update/up_script.sql
@@ -3,3 +3,5 @@
 	you can use "\i 'function/reclada_object.get_schema.sql'"
 	to run text script of functions
 */
+CREATE EXTENSION IF NOT EXISTS aws_lambda CASCADE;
+\i 'function/api.storage_generate_presigned_get.sql'
