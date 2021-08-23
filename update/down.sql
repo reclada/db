9d5720a10e9e033d43c52fae62be4704b7e7cf59
AlexandraVin <alex.vingert@gmail.com>
2021-08-23 17:32:40 +0300

    * improve api.storage_generate_presigned_get with Lambda function

--- update/down.sql
+++ update/down.sql
@@ -1,2 +1,4 @@
 -- you you can use "--{function/reclada_object.get_schema}"
 -- to add current version of object to downgrade script
+DROP EXTENSION IF EXISTS aws_lambda CASCADE;
+--{function/api.storage_generate_presigned_get}
