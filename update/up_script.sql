788f3ad5b53ba099f9940caaf3cd85d5acb539ff
EvVa <EvVa>
2021-08-27 15:58:37 +0300

    add upgrage datasource_insert_trigger_fnc from version 4

--- update/up_script.sql
+++ update/up_script.sql
@@ -8,3 +8,4 @@
 \i 'function/reclada_object.list_related.sql'
 \i 'function/api.reclada_object_list.sql'
 \i 'function/api.reclada_object_list_related.sql'
+\i 'reclada.datasource_insert_trigger_fnc.sql'
