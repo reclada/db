40f604887d7ddc262e0c4dfa8fe9ec54264ee151
EvVa <EvVa>
2021-09-07 16:23:50 +0300

    version = 16 improve create for work with existing guid create constraint (revision, GUID)

--- update/up_script.sql
+++ update/up_script.sql
@@ -1,7 +1,12 @@
--- version = 15
+-- version = 16
 /*
     you can use "\i 'function/reclada_object.get_schema.sql'"
     to run text script of functions
 */
 
-\i 'function/reclada_object.list.sql'
+\i 'reclada_object.create.sql'
+
+
+CREATE UNIQUE INDEX unique_guid_revision 
+    ON reclada.object((attributes->>'revision'),obj_id);
+
