7f58ab444b9d6c03cf5ba2406c3d14a0d5f0b616
AlexandraVin <alex.vingert@gmail.com>
2021-09-08 14:10:41 +0300

    * version = 17 * improve load_staging trigger

--- update/down.sql
+++ update/down.sql
@@ -1,11 +1,4 @@
 -- you you can use "--{function/reclada_object.get_schema}"
 -- to add current version of object to downgrade script
 
---{function/reclada_object.create}
-
-CREATE TABLE reclada.staging(
-    data    jsonb   NOT NULL
-);
-\i 'function/reclada.load_staging'
-\i 'view/reclada.staging'
-\i 'trigger/load_staging'
+SELECT public.raise_notice('Downscript is not supported');
