8f3c467a991a0612951cfbe83325bd95be8ef487
EvVa <EvVa>
2021-09-27 13:46:57 +0300

    improve api.reclada_object_get_transaction_id name input parametr

--- update/function/api.reclada_object_get_transaction_id.sql
+++ update/function/api.reclada_object_get_transaction_id.sql
@@ -1,11 +1,11 @@
 drop FUNCTION if exists api.reclada_object_get_transaction_id;
-CREATE OR REPLACE FUNCTION api.reclada_object_get_transaction_id(_data JSONB)
+CREATE OR REPLACE FUNCTION api.reclada_object_get_transaction_id(data JSONB)
 RETURNS JSONB AS $$
 DECLARE
     _action text;
     _res jsonb;
     _guid text;
 BEGIN
-    return reclada_object.get_transaction_id(_data);
+    return reclada_object.get_transaction_id(data);
 END;
 $$ LANGUAGE PLPGSQL VOLATILE;
