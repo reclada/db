4892a50ab0db9666efd49fd2ee98fd61bf436d0d
Nikolay Kukhtenko <nikolay.kukhtenko@quantori.com>
2021-10-07 14:31:14 +0300

    Added downgrade script

--- update/down.sql
+++ update/down.sql
@@ -1,4 +1,40 @@
 -- you you can use "--{function/reclada_object.get_schema}"
 -- to add current version of object to downgrade script
 
-select reclada.raise_exception('Downgrade script not support');
+
+--{function/reclada.datasource_insert_trigger_fnc}
+
+create trigger datasource_insert_trigger before
+insert
+    on
+    reclada.object for each row execute function datasource_insert_trigger_fnc();
+
+DROP VIEW IF EXISTS reclada.v_revision;
+DROP VIEW IF EXISTS reclada.v_import_info;
+DROP VIEW IF EXISTS reclada.v_pk_for_class;
+DROP VIEW IF EXISTS reclada.v_class;
+DROP VIEW IF EXISTS reclada.v_active_object;
+DROP VIEW IF EXISTS reclada.v_object;
+
+DROP MATERIALIZED VIEW IF EXISTS reclada.v_object_status;
+DROP MATERIALIZED VIEW IF EXISTS reclada.v_user;
+DROP MATERIALIZED VIEW IF EXISTS reclada.v_class_lite;
+
+--{view/reclada.v_class_lite}
+--{view/reclada.v_object_status}
+--{view/reclada.v_user}
+
+--{view/reclada.v_object}
+--{view/reclada.v_active_object}
+--{view/reclada.v_class}
+--{view/reclada.v_pk_for_class}
+--{view/reclada.v_import_info}
+--{view/reclada.v_revision}
+
+DROP FUNCTION reclada_object.refresh_mv;
+DROP FUNCTION reclada_object.datasource_insert;
+
+--{function/reclada_object.create_subclass}
+--{function/reclada_object.create}
+--{function/reclada_object.update}
+--{function/reclada_object.delete}
