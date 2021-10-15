2dd29e6154685620d88aec1a260e50736f584d9c
EvVa <EvVa>
2021-10-15 11:23:36 +0300

    done commit_prepare.py improved install_db.py

--- update/install_db.py
+++ update/install_db.py
@@ -82,10 +82,10 @@ if __name__ == "__main__":
     
     recreate_db()
     need_update, use_dump = db_install()
-    if run_object_create:
-        install_objects()
     if need_update:
         os.system('python update_db.py')
         if not use_dump:
             scinlp_install()
             runtime_install()
+    if run_object_create:
+        install_objects()
