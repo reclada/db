b5f992cf5679e6e045bd16e9f82a8e515f6f5ecd
EvVa <EvVa>
2021-10-28 13:59:53 +0300

    add platformRunnerID to Runner

--- update/down.sql
+++ update/down.sql
@@ -20,3 +20,67 @@ DROP VIEW IF EXISTS reclada.v_object;
 --{view/reclada.v_revision}
 --{function/reclada.xor}
 
+update reclada.object
+    set attributes = '
+{
+    "schema": {
+        "type": "object",
+        "required": [
+            "command",
+            "status",
+            "type",
+            "task",
+            "environment"
+        ],
+        "properties": {
+            "tags": {
+                "type": "array",
+                "items": {
+                    "type": "string"
+                }
+            },
+            "task": {
+                "type": "string",
+                "pattern": "[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}"
+            },
+            "type": {
+                "type": "string"
+            },
+            "runner": {
+                "type": "string",
+                "pattern": "[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}"
+            },
+            "status": {
+                "type": "string",
+                "enum ": [
+                    "up",
+                    "down",
+                    "idle"
+                ]
+            },
+            "command": {
+                "type": "string"
+            },
+            "environment": {
+                "type": "string",
+                "pattern": "[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}"
+            },
+            "inputParameters": {
+                "type": "array",
+                "items": {
+                    "type": "object"
+                }
+            },
+            "outputParameters": {
+                "type": "array",
+                "items": {
+                    "type": "object"
+                }
+            }
+        }
+    },
+    "version": "1",
+    "forClass": "Runner"
+}'::jsonb
+    where class = reclada_object.get_jsonschema_GUID() 
+        and attributes->>'forClass' = 'Runner';
