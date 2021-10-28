1d6b6d68e30f8d57fbf70aaf29509c3908ef2a64
Maksim Vasilev <maksim.vasilev@quantori.com>
2021-10-28 12:05:17 +0300

    1 debug

--- update/update_db.py
+++ update/update_db.py
@@ -98,7 +98,7 @@ def rmdir(top:str):
 
 def clone_db():
     rmdir('db')
-    os.system(f'git clone https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.reclada.com/developers/db.git')
+    os.system(f'git clone https://gitlab-ci-token:$CI_JOB_TOKEN@gitlab.reclada.com/developers/db.git')
     os.chdir('db')
     checkout(branch_db)
 
