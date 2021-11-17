12da1424114ef01e5eed05cda60e0831cf020e01
EvVa <EvVa>
2021-11-17 19:36:20 +0300

    improve need_flat

--- update/function/reclada_object.need_flat.sql
+++ update/function/reclada_object.need_flat.sql
@@ -7,17 +7,12 @@
 DROP FUNCTION IF EXISTS reclada_object.need_flat;
 CREATE OR REPLACE FUNCTION reclada_object.need_flat(_class_name text)
 RETURNS bool AS $$
-    select r as res 
-        from
+    select exists
         (
             select true as r
                 from reclada.v_object_display d
                 join reclada_object.get_GUID_for_class(_class_name) tf
                     on tf.obj_id = d.class_guid
                 where d.table is not null
-            union 
-            select false as r
-        ) t
-        order by r desc 
-        limit 1
+        )
 $$ LANGUAGE SQL STABLE;
