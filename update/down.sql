ece01da4da01245b93faea98401e666553c3ae6f
AlexandraVin <alex.vingert@gmail.com>
2022-01-24 15:36:23 +0300

    * updated up and down scripts with old 47 version

--- update/down.sql
+++ update/down.sql
@@ -2,6 +2,21 @@
 -- to add current version of object to downgrade script
 
 
+drop table reclada.unique_object_reclada_object;
+drop table reclada.unique_object;
+drop table reclada.field;
+
+--{function/reclada_object.create}
+--{function/reclada_object.get_schema}
+--{view/reclada.v_ui_active_object}
+--{function/reclada_object.update}
+--{function/reclada_object.list}
+--{function/reclada.update_unique_object}
+--{function/reclada.random_string}
+--{function/api.reclada_object_list}
+--{view/reclada.v_filter_mapping}
+
+
 --------------default----------------
 UPDATE reclada.object
 SET attributes = '{
--- update/up_script.sql
+++ update/up_script.sql
@@ -1,9 +1,62 @@
--- version = 46
+-- version = 47
 /*
     you can use "\i 'function/reclada_object.get_schema.sql'"
     to run text script of functions
 */
 
+
+
+create table reclada.field
+(
+    id      bigint
+        NOT NULL
+        GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1)
+        UNIQUE ,
+    path      text
+        NOT NULL,
+    json_type text
+        NOT NULL,
+    PRIMARY KEY (path, json_type)
+);
+
+create table reclada.unique_object
+(
+    id bigint
+        NOT NULL
+        GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1)
+        UNIQUE,
+    id_field bigint[]
+        NOT NULL,
+    PRIMARY KEY (id_field)
+);
+
+create table reclada.unique_object_reclada_object
+(
+    id bigint
+        NOT NULL
+        GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1),
+    id_unique_object    bigint
+        NOT NULL
+        REFERENCES reclada.unique_object(id),
+    id_reclada_object    bigint
+        NOT NULL
+        REFERENCES reclada.object(id) ON DELETE CASCADE,
+    PRIMARY KEY(id_unique_object,id_reclada_object)
+);
+
+\i 'function/reclada_object.create.sql'
+\i 'function/reclada_object.update.sql'
+\i 'function/reclada_object.list.sql'
+\i 'function/reclada_object.get_schema.sql'
+\i 'function/reclada.update_unique_object.sql'
+\i 'function/reclada.random_string.sql'
+\i 'function/api.reclada_object_list.sql'
+\i 'view/reclada.v_ui_active_object.sql'
+\i 'view/reclada.v_filter_mapping.sql'
+\i 'view/reclada.v_ui_active_object.sql'
+
+
+select reclada.update_unique_object(null, true);
 
 --------------default----------------
 
