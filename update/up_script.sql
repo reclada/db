fd9e173eb381cb521d9db2a1236ef4ee22fa88b6
EvVa <EvVa>
2022-01-31 09:11:35 +0300

    fix migration

--- update/up_script.sql
+++ update/up_script.sql
@@ -1,63 +1,11 @@
--- version = 47
+-- version = 48
 /*
     you can use "\i 'function/reclada_object.get_schema.sql'"
     to run text script of functions
 */
 
-create table reclada.field
-(
-    id      bigint
-        NOT NULL
-        GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1)
-        UNIQUE ,
-    path      text
-        NOT NULL,
-    json_type text
-        NOT NULL,
-    PRIMARY KEY (path, json_type)
-);
-
-create table reclada.unique_object
-(
-    id bigint
-        NOT NULL
-        GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1)
-        UNIQUE,
-    id_field bigint[]
-        NOT NULL,
-    PRIMARY KEY (id_field)
-);
-
-create table reclada.unique_object_reclada_object
-(
-    id bigint
-        NOT NULL
-        GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1),
-    id_unique_object    bigint
-        NOT NULL
-        REFERENCES reclada.unique_object(id),
-    id_reclada_object    bigint
-        NOT NULL
-        REFERENCES reclada.object(id) ON DELETE CASCADE,
-    PRIMARY KEY(id_unique_object,id_reclada_object)
-);
-
 -- \i 'function/reclada_object.update.sql'
-\i 'function/reclada_object.list.sql'
-\i 'function/reclada_object.get_schema.sql'
-\i 'function/reclada_object.explode_jsonb.sql'
-\i 'function/reclada.update_unique_object.sql'
-\i 'function/reclada.random_string.sql'
-\i 'function/api.reclada_object_list.sql'
-\i 'view/reclada.v_ui_active_object.sql'
-\i 'function/reclada.get_duplicates.sql'
-\i 'function/reclada_object.refresh_mv.sql'
-\i 'view/reclada.v_filter_mapping.sql'
-\i 'view/reclada.v_get_duplicates_query.sql'
-\i 'function/reclada.get_unifield_index_name.sql'
 
-
-select reclada.update_unique_object(null, true);
 
 --{ REC-564
     \i 'view/reclada.v_component.sql'
