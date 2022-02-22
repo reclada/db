5d9aa6a6e9334239a0ed6747199bd12bf6851f03
Nikita Kryuchkovoy <nikita.kryuchkovoy@quantori.com>
2022-02-22 16:41:11 +0300

    final change for object.list first change for load.staging

--- update/function/reclada.load_staging.sql
+++ update/function/reclada.load_staging.sql
@@ -1,8 +1,24 @@
-CREATE OR REPLACE FUNCTION reclada.load_staging()
-RETURNS TRIGGER
+CREATE OR REPLACE FUNCTION reclada.load_staging() 
+RETURNS TRIGGER 
 AS $$
+DECLARE
+    _data_agg jsonb;
+    _batch_size bigint;
+    _length_data bigint;
 BEGIN
-    PERFORM reclada_object.create(NEW.data);
+    _batch_size := 1000;
+    SELECT COUNT(data)
+        FROM NEW_TABLE
+        INTO _length_data;
+    --add row_number() over(order by data) as id
+    FOR i IN 1.._length_data / _batch_size LOOP -- refact
+        SELECT jsonb_agg(data)
+            FROM NEW_TABLE
+            WHERE (i - 1)  * 1000 < id <= i * 1000
+            INTO _data_agg;
+        PERFORM reclada_object.create(_data_agg);
+    END LOOP;
     RETURN NEW;
+    TRUNCATE TABLE NEW_TABLE;
 END
 $$ LANGUAGE PLPGSQL VOLATILE;
