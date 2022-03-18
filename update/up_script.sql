69cd1b33fd2a7543dbcac57c0932f869dff92b28
Nikita Kryuchkovoy <nikita.kryuchkovoy@quantori.com>
2022-03-18 18:01:58 +0300

    init v_50

--- update/up_script.sql
+++ update/up_script.sql
@@ -1,19 +1,9 @@
--- version = 49
+-- version = 50
 /*
     you can use "\i 'function/reclada_object.get_schema.sql'"
     to run text script of functions
 */
 
-alter table dev.component add parent_component_name text;
-
-create table dev.meta_data(
-    id bigint
-        NOT NULL
-        GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1)
-        UNIQUE ,
-    ver bigint,
-    data jsonb
-);
 
 \i 'function/reclada_object.list.sql'
 \i 'function/reclada.load_staging.sql'
