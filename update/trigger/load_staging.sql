8cc2b89bb22d725fdb56f7459bf4edc4bd577186
EvVa <EvVa>
2022-04-22 19:48:11 +0300

    TRIGGER fix

--- update/trigger/load_staging.sql
+++ update/trigger/load_staging.sql
@@ -2,4 +2,4 @@ DROP TRIGGER IF EXISTS load_staging ON reclada.staging;
 CREATE TRIGGER load_staging
     AFTER INSERT ON reclada.staging
     REFERENCING NEW TABLE AS NEW_TABLE
-    FOR EACH STATEMENT EXECUTE FUNCTION load_staging();
+    FOR EACH STATEMENT EXECUTE FUNCTION reclada.load_staging();
